1000c845b6c181d635c324824a058eb5db7d1813
taba <tabatabaeipour.sa@gmail.com>
2021-08-11 22:03:40 +0430

    add base.html and index.html

--- MyStore/MyStore/urls.py
+++ MyStore/MyStore/urls.py
@@ -2,10 +2,12 @@
 from django import urls
 from django.contrib import admin
 from django.urls import path , include
+from . import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('' , views.index ),
     path('about-us/' , include('about.urls') ),
     path('contact-us/' , include('contact.urls'))
 ]
