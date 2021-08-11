b0a1c53b59b28ec6037bcc8ea11f10c859869dea
taba <tabatabaeipour.sa@gmail.com>
2021-08-11 17:14:00 +0430

    start a new app: about.

--- MyStore/MyStore/settings.py
+++ MyStore/MyStore/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'about',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'MyStore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
