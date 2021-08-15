96bc0cac5d49eb13eb9a3d366699e0f813f75842
taba <tabatabaeipour.sa@gmail.com>
2021-08-15 14:43:50 +0430

    create function for login

--- MyStore/accounts/urls.py
+++ MyStore/accounts/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login' , views.login ),
+    path('login' , views.login_view ),
     path('signup' , views.signup_view)
 ]
--- MyStore/accounts/views.py
+++ MyStore/accounts/views.py
@@ -1,11 +1,19 @@
 from django.forms.forms import Form
 from django.shortcuts import redirect, render
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login
 # Create your views here.
 
-def login (request):
-    pass
+def login_view (request):
+    if request.method == 'POST' :
+        form = AuthenticationForm(data = request.POST)
+        if form.is_valid():
+            user = form.save()
+            login (request , user)
+            return redirect('index:index')
+    else :
+        form = AuthenticationForm()
+    return render( request , 'accounts/login.html' , {'form' : form} )
 
 
 def signup_view (request):
